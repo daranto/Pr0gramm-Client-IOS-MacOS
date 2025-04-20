1772f563eef4b7cd47d36ba6a36eadc85f2d8984
Daranto <xcode@x0g.de>
2025-04-20 21:59:22 +0200

    Initial Commit

---
+++ Pr0gramm/Pr0grammApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pr0grammApp.swift
+//  Pr0gramm
+//
+//  Created by Robin Schmidt on 20.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Pr0grammApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
